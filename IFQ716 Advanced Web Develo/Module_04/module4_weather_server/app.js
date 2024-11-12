7bd87854ba6f876596e088c96030751c01cfc37a
ChristopherNHorsfall <113425702+ChristopherNHorsfall@users.noreply.github.com>
2024-11-12 16:11:29 +1000

    mod 4 cache data

--- IFQ716 Advanced Web Develo/Module_04/module4_weather_server/app.js
+++ IFQ716 Advanced Web Develo/Module_04/module4_weather_server/app.js
@@ -4,14 +4,44 @@ const http = require("http");
 const WEATHERAPI_BASE = "http://api.weatherapi.com/v1";
 const API_KEY = process.env.WEATHERAPI_KEY;
 
-async function weather(res) {
-    const weatherResponse = await fetch(`${WEATHERAPI_BASE}/current.json?key=${API_KEY}&q=Brisbane`);
-    const weatherData = await weatherResponse.json();
-    const responseData = {"condition":weatherData.current.condition.text, "temperature": weatherData.current.temp_c};
+// Global variables for caching
+let cachedWeatherData = null;
+let lastAccessTime = 0
 
-    res.writeHead(200, {"Content-Type": "application/json", "Access-Control-Allow-Origin":"*"});
-    res.write(JSON.stringify(responseData));
-    res.end();
+async function weather(res) {
+    const currentTime = new Date().getTime();
+
+    // Check if we have cached data and it's still valid (within 30 seconds)
+    if (cachedWeatherData && currentTime - lastAccessTime < 30000){
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(cachedWeatherData));
+        return;
+    }
+    try {
+        // Fetch data from the Weather API
+        const weatherResponse = await fetch(`${WEATHERAPI_BASE}/current.json?key=${API_KEY}&q=Brisbane`);
+        const weatherData = await weatherResponse.json();
+
+        // Extract necessary data for response
+        const responseData = {"condition":weatherData.current.condition.text, "temperature": weatherData.current.temp_c};
+        
+        // Update cache
+        cachedWeatherData = {
+            condition: weatherData.current.condition,
+            temperature: weatherData.current.temp_c
+        };
+        lastAccessTime = currentTime;
+
+        // Send response with new data
+        res.writeHead(200, {"Content-Type": "application/json", "Access-Control-Allow-Origin":"*"});
+        res.write(JSON.stringify(responseData));
+        res.end();
+
+    } catch(error) {
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: 'Error fetching weather data' }));
+    }
+
 }
 
 function routing(req, res) {
